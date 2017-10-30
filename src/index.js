6b405e871005bbf6b3ca9c698ca619087f1548b1
Janis Miezitis <janjiss@gmail.com>
2017-10-30 19:58:59 +0200

    Added completed version of Todo

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ require("./styles.scss");
 
 var Elm = require('./Todo');
 
-var todomvc = Elm.Todo.fullscreen();
+var storedState = localStorage.getItem('elm-todo-save');
+var startingState = storedState ? JSON.parse(storedState) : null;
+
+var todomvc = Elm.Todo.fullscreen(startingState);
+
+todomvc.ports.setStorage.subscribe(function(state) {
+    localStorage.setItem('elm-todo-save', JSON.stringify(state));
+});
